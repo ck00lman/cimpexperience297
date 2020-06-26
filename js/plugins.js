1bc66b71567521b1d2eac78dcf00ea60b32ce1c0
Christopher Koolman <ckoolman1985@gmail.com>
2020-06-25 22:11:36 -0400

    Updated Reviews/Testimonials section to Promotions in order to add a Bootstrap image slider carousel to make the website more interactive

--- js/plugins.js
+++ js/plugins.js
@@ -1,5 +1,4 @@
 /*========== CLOSE MOBILE NAV ON CLICK ==========*/
-
 $(document).ready(function () { //when document loads completely.
   $(document).click(function (event) { //click anywhere
       var clickover = $(event.target); //get the target element where you clicked
@@ -10,35 +9,6 @@ $(document).ready(function () { //when document loads completely.
   });
 });
 
-
-/*========== SMOOTH SCROLLING TO LINKS ==========*/
-
-$(document).ready(function(){ //document is loaded
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {//click on any link;anchor tag;
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") { //for e.g. website.com#home - #home
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-      //console.log('hash:',hash)
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({ //animate whole html and body elements
-        scrollTop: $(hash).offset().top //scroll to the element with that hash
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash; //website.com - website.com#home
-        //Optional remove "window.location.hash = hash;" to prevent transparent navbar on load
-      });
-    } // End if
-  });
-});
 
 
 /*========== LIGHTBOX GALLERY SETTINGS
@@ -52,51 +22,3 @@ $(document).ready(function(){
   })
 });
 
-
-/*========== CONTACT FORM INPUT VALIDATION ==========*/
-
-$(function () {
-
-  // init the validator
-  // validator files are included in the download package
-  // otherwise download from http://1000hz.github.io/bootstrap-validator
-
-  $('#contact-form').validator();
-
-
-  // when the form is submitted
-  $('#contact-form').on('submit', function (e) {
-
-      // if the validator does not prevent form submit
-      if (!e.isDefaultPrevented()) {
-          var url = "contact/contact.php"; //Location of form (apply change if moved).
-
-          // POST values in the background the the script URL
-          $.ajax({
-              type: "POST",
-              url: url,
-              data: $(this).serialize(),
-              success: function (data)
-              {
-                  // data = JSON object that contact.php returns
-
-                  // we recieve the type of the message: success x danger and apply it to the 
-                  var messageAlert = 'alert-' + data.type;
-                  var messageText = data.message;
-
-                  // let's compose Bootstrap alert box HTML
-                  var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                  
-                  // If we have messageAlert and messageText
-                  if (messageAlert && messageText) {
-                      // inject the alert to .messages div in our form
-                      $('#contact-form').find('.messages').html(alertBox);
-                      // empty the form
-                      $('#contact-form')[0].reset();
-                  }
-              }
-          });
-          return false;
-      }
-  })
-});
